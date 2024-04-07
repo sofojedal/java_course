101de2cef25bf158cdf1dedc41cc612abb98c4d5
Laura Sofía Ojeda León <111465717+sofojedal@users.noreply.github.com>
2024-04-07 16:38:32 -0500

    Create PasarPorValor.java

---
+++ PasarPorValor.java
@@ -0,0 +1,14 @@
+public class PasarPorValor {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("iniciamos el método main con i = " + i);
+        test(i);
+        System.out.println("Finaliza el método main con el valor de i = " + i);
+    }
+
+    public static void test(int i){
+        System.out.println("Iniciamos el método test con i = " + i);
+        i = 35;
+        System.out.println("Finaliza el método test con i = " + i);
+    }
+}
