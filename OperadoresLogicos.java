6ff78178c0cb6956c4065b07c421879f4a0daea2
Laura Sofía Ojeda León <111465717+sofojedal@users.noreply.github.com>
2024-04-07 15:47:10 -0500

    Create OperadoresLogicos.java

---
+++ OperadoresLogicos.java
@@ -0,0 +1,37 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int i = 3;
+        byte j = 3;
+        float k = 127e-7f;
+        double l = 2.1413e3;
+        boolean m = false;
+        //Relacion de igualdad
+        //Devuelve un valor booleano
+        //Llega hasta el primer criterio ya que && (and) evalua que si uno es falso, entonces toda la expresion es falsa
+        // && si una es verdadera, ahi si evalua la siguiente
+        // Como m es false, entonces toda la expresion va a ser falsa
+        boolean b1 = i == j && k < l && m == false;
+        System.out.println("b1 = " + b1);
+
+        //or, si al menos una se cumple, es true
+        boolean b2 = i == j || k < l;
+        System.out.println("b2 = " + b2);
+
+        //(Se cambio la prioridad), al menos uno es true
+        boolean b3 = i == j && (k < l || m == false);
+        System.out.println("b3 = " + b3);
+
+        //AND tiene prioridad sobre el OR
+        boolean b4 = i == j || (k < l && m == true);
+        System.out.println("b4 = " + b4);
+
+        boolean b5 = (true || true) && false;
+        System.out.println("b5 = " + b5);
+
+        boolean b6 = ((true || false) && false) || false;
+        System.out.println("b6 = " + b6);
+
+        boolean b7 = ((true || false) && false) || false;
+        System.out.println("b7 = " + b7);
+    }
+}
