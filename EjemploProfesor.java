42ca5b54a2f445b59cad35de45e96c26633a9ab7
Laura Sofía Ojeda León <111465717+sofojedal@users.noreply.github.com>
2024-10-02 14:20:44 -0500

    Create EjemploProfesor.java

---
+++ EjemploProfesor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EjemploProfesor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double area; // el área a calcular
+
+        double radio; // radio a ingresar
+        System.out.println("Introduce el radio de un circulo: ");
+        radio = scanner.nextDouble();
+        area = Math.PI * Math.pow(radio, 2);
+
+        System.out.println("El área de una circunferencia de radio " + radio + " es: " + area);
+    }
+}
